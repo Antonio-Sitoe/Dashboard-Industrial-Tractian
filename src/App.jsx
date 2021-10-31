59751ba9d3fd714fcf9523b962c59f09bb46352d
Antonio Sitoe <72309855+Antonio-Sitoe@users.noreply.github.com>
2021-10-31 14:09:41 +0200

    ultimo server

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyles/GlobalStyles';
 import Dashboard from './Components/Dashboad/Dashboard';
 import Page404 from './Components/Page404/Page404';
-import Login from './Components/Login/Login'
-import {UserStorage} from './Context/UserContext'
+import Login from './Components/Login/Login';
+import { UserStorage } from './Context/UserContext';
 
 function App() {
   return (
-    <BrowserRouter>
-    <UserStorage>
-      <Routes>
-        <Route path='/*' element={<Login/>} />
-        <Route path='dashboard/*' element={<Dashboard />} />
-        <Route path='*' element={<Page404 />} />
-      </Routes>
-      <GlobalStyle />
+    <HashRouter>
+      <UserStorage>
+        <Routes>
+          <Route path='/*' element={<Login />} />
+          <Route path='dashboard/*' element={<Dashboard />} />
+          <Route path='*' element={<Page404 />} />
+        </Routes>
+        <GlobalStyle />
       </UserStorage>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
